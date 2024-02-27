fd73b2e393920e8cf6527216b159cd0b0d8805b1
Amaury Le Roux <amaury.le-roux@universite-paris-saclay.fr>
2024-02-27 19:00:15 +0100

    Mise en place du Bot (fonctionnel)

--- Bot-Discord.py
+++ Bot-Discord.py
@@ -1,3 +1,45 @@
-print("hello World!")
+import discord
+from discord.ext  import commands
 
-print("félicitation, youpi, merveilleux")
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+
+# message de connexion
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user.name}')
+
+# Deconnexion du bot
+@bot.command(name="exit")
+async def exit(ctx):
+
+    reponse = "Bot déconnecté. Bye Bye !"
+    await ctx.reply(reponse)
+    await bot.close()
+
+    print(f"Réponse à message {ctx.message.id} : {reponse}")
+
+# fonction de test
+@bot.command(name="coucou")
+async def bonjour(ctx):
+
+    reponse = f"Ça va, {ctx.message.author.name} ?"
+    await ctx.reply(reponse)
+
+    # print(f"Réponse à message {ctx.message.id} : {reponse}")
+
+@bot.command(name="somme")
+async def somme(ctx,val1:int,val2:int):
+
+    reponse = f"{val1} + {val2} = {val1+val2}"
+    await ctx.reply(reponse)
+
+    # print(f"Réponse à message {ctx.message.id} : {reponse}")
+
+
+bot.run("TOKEN") # Rentrer son token
